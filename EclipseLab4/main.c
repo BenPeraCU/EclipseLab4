c435bc1c1d945a93a2e4a356ec00891a4e87a011
BenPeraCU <73373478+BenPeraCU@users.noreply.github.com>
2021-02-24 12:41:17 -0700

    pwm actually works now through interrupt

--- EclipseLab4/main.c
+++ EclipseLab4/main.c
@@ -1,5 +1,5 @@
 #include "msp.h"
-#define TICKS ((uint16_t)0x493E)
+#define TICKS ((uint16_t)0x493F)
 
 /**
  * main.c
@@ -14,13 +14,13 @@ void timerA_config(void){
     TIMER_A0->CTL       |= TIMER_A_CTL_SSEL__SMCLK; //Use SMCLK
     TIMER_A0->CTL       |= TIMER_A_CTL_ID_2; // Sets timer ID to 2 ---- division by 4
     TIMER_A0->CCR[0]    = TICKS;
-    TIMER_A0->CCR[1]    = TICKS -10;
+    TIMER_A0->CCR[1]    = TICKS - 10;
     TIMER_A0->CCTL[1]   |= TIMER_A_CCTLN_CCIE;
+    //TIMER_A0->CCTL[1]   |= TIMER_A_CCTLN_OUTMOD_7; //Resets Output
+    //TIMER_A0->CCTL[1]   &= TIMER_A_CCTLN_OUTMOD_4; // Sets output to toggle
 }
 
 void timerA_start(void){
-    //TIMER_A0->CCTL[1]   |= TIMER_A_CCTLN_OUTMOD_7; //Resets Output
-    //TIMER_A0->CCTL[1]   &= TIMER_A_CCTLN_OUTMOD_4; // Sets output to toggle
     TIMER_A0->CTL |= TIMER_A_CTL_MC__UP;
 }
 
@@ -30,9 +30,9 @@ void config_NVIC(void){
 
 void gpio_config(void){
     P2->DIR     |= BIT4;
-    P2->OUT     &= ~BIT4;                    // Make sure that P2 is an output
+    P2->OUT     |= BIT4;                    // Make sure that P2 is an output
     //P2->DS     = BIT4;                    // Sets drive strength to high
-    P2->SEL0    |= BIT4;                    // Select the mode
+    P2->SEL0    &= ~BIT4;                    // Select the mode
     P2->SEL1    &= ~(BIT4);
 }
 
