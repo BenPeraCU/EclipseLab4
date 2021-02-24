e0b78fb88bf317d0bc9859ea3a26d8206a2e8aa8
BenPeraCU <73373478+BenPeraCU@users.noreply.github.com>
2021-02-24 13:18:42 -0700

    got pretty far

--- EclipseLab4/main.c
+++ EclipseLab4/main.c
@@ -5,17 +5,34 @@
  * main.c
  */
 
+
+volatile uint16_t CaptureValues [2] = {0};
+
 void timerA_stop(void){
     TIMER_A0->CTL &= TIMER_A_CTL_MC__STOP;
 }
 
 void timerA_config(void){
+
+    //Configuring clock
     TIMER_A0->CTL       |= TIMER_A_CTL_CLR; // clears TimerA0
     TIMER_A0->CTL       |= TIMER_A_CTL_SSEL__SMCLK; //Use SMCLK
     TIMER_A0->CTL       |= TIMER_A_CTL_ID_2; // Sets timer ID to 2 ---- division by 4
     TIMER_A0->CCR[0]    = TICKS;
-    TIMER_A0->CCR[1]    = TICKS - 10;
-    TIMER_A0->CCTL[1]   |= TIMER_A_CCTLN_CCIE;
+    TIMER_A0->CCR[1]    = TICKS;
+
+
+    //configuring interrupts
+    TIMER_A0->CCTL[1]   |= TIMER_A_CCTLN_CCIE;  //Enable interrupt for Trigger on sensor - P2.4
+    TIMER_A0->CCTL[2]   |= TIMER_A_CCTLN_CCIE;  //Enable interrupt for  Echo on sensor - P2.5
+
+    TIMER_A0->CCTL[2]   |= TIMER_A_CCTLN_CM__BOTH;   //set as capture input on rising and falling edge
+    TIMER_A0->CCTL[2]   |= TIMER_A_CCTLN_CAP;   //set as capture mode
+
+    TIMER_A0->CCTL[1]   &= ~TIMER_A_CCTLN_CCIFG; //clear interrupt flag
+    TIMER_A0->CCTL[2]   &= ~TIMER_A_CCTLN_CCIFG; //clear interrupt flag
+
+
     //TIMER_A0->CCTL[1]   |= TIMER_A_CCTLN_OUTMOD_7; //Resets Output
     //TIMER_A0->CCTL[1]   &= TIMER_A_CCTLN_OUTMOD_4; // Sets output to toggle
 }
@@ -34,16 +51,43 @@ void gpio_config(void){
     //P2->DS     = BIT4;                    // Sets drive strength to high
     P2->SEL0    &= ~BIT4;                    // Select the mode
     P2->SEL1    &= ~(BIT4);
+
+    P2->DIR     &= ~BIT5;           //configure P2.5 to input
+    P2->OUT     &= ~BIT5;           //configure pull down on P2.5
+    P2->REN     |= BIT5;            //enable pull down
+
+    P2->SEL0    |=  BIT5;           //enable primary module function on P2.5 (TIMER_A0 CCR2)
+    P2->SEL1    &=  ~BIT5;
 }
 
 void TA0_N_IRQHandler(void){
     __NVIC_DisableIRQ(TA0_N_IRQn); //disable since we're in the interrupt
 
-    //Toggle P2.4
-    P2->OUT ^= BIT4;
 
-    // Clear the Interrupt Source Flag
-    TIMER_A0->CCTL[1] &= ~TIMER_A_CCTLN_CCIFG;
+    //if interrupt is on CCR[1] toggle the output
+    if(TIMER_A0->CCTL[1] & TIMER_A_CCTLN_CCIFG){
+        //Toggle P2.4
+        P2->OUT ^= BIT4;
+
+        // Clear the Interrupt Source Flag
+        TIMER_A0->CCTL[1] &= ~TIMER_A_CCTLN_CCIFG;
+    }
+
+    if(TIMER_A0->CCTL[2] & TIMER_A_CCTLN_CCIFG){
+
+        if(P2->IN & BIT5){
+            CaptureValues[0] = TIMER_A0->CCR[2];
+            TIMER_A0->CCTL[2] &= ~TIMER_A_CCTLN_COV;    //clear Overflow
+        }
+        if(~(P2->IN & BIT5)){
+            CaptureValues[1] = TIMER_A0->CCR[2];
+            if(TIMER_A0->CCTL[2] & TIMER_A_CCTLN_COV){
+                //error happened plz implement
+            }
+        }
+        // Clear the Interrupt Source Flag
+        TIMER_A0->CCTL[2] &= ~TIMER_A_CCTLN_CCIFG;
+    }
 
     __NVIC_EnableIRQ(TA0_N_IRQn); //enable interrupt since we are about to exit handler
 }
@@ -58,6 +102,8 @@ void main(void)
 	timerA_start();
 	gpio_config();
 	config_NVIC();
+
+
 
 	while(1){
 	}
